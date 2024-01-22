0e5247d0bc31e8a092cbfa232f6e16bcfa64dd34
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-01-22 21:08:05 +0000

    new

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -11,21 +11,5 @@
 library(tidyverse)
 
 #### Clean data ####
-raw_data <- read_csv("inputs/data/plane_data.csv")
+the_raw_data_crime_victims <- read_csv("inputs/data/plane_data.csv")
 
-#### Basic cleaning ####
-raw_elections_data <-
-  read_csv(
-    file = "australian_voting.csv",
-    show_col_types = FALSE
-  )
-# Make the names easier to type
-cleaned_elections_data <-
-  clean_names(raw_elections_data)
-
-# Have a look at the first six rows
-head(cleaned_elections_data)
-  tidyr::drop_na()
-
-#### Save data ####
-write_csv(cleaned_data, "outputs/data/analysis_data.csv")
