05f6e19822aed9e4053750e3813dced8da0ba622
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-01-23 02:12:42 +0000

    Data cleaned for use

--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -1,15 +1,43 @@
 #### Preamble ####
-# Purpose: Cleans the raw plane data recorded by two observers..... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 6 April 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Cleans the raw data obtained from Open Data Toronto for victim of crimes.
+# Author: Mohammed Yusuf Shaikh
+# Date: 22 January 2024
+# Contact: mohammedyusuf.shaikh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: Run 01-download_data.R first from script.
 
 #### Workspace setup ####
 library(tidyverse)
+library(janitor)
+library(dplyr)
 
 #### Clean data ####
-the_raw_data_crime_victims <- read_csv("inputs/data/plane_data.csv")
+the_raw_data_crime_victims <- read_csv("inputs/data/raw_data.csv", show_col_types = FALSE)
+
+# Search by giving column names
+names(the_raw_data_crime_victims)
+
+# Removing 2 columns as the data focuses on the crime type
+cleaned_victim_data <- the_raw_data_crime_victims %>%
+  select(-CATEGORY, -ASSAULT_SUBTYPE)
+
+# Make the names easier to type
+cleaned_victim_data <-
+  clean_names(cleaned_victim_data)
+
+# Rename count column
+cleaned_victim_data <-
+  cleaned_victim_data |>
+  rename(
+    victim_count = count
+  )
+
+# A look at the first six rows
+head(cleaned_victim_data)
+
+
+# Save data in inputs/data
+write_csv(cleaned_victim_data, file= "inputs/data/cleaned_data.csv")
+
+
 
