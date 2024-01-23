6a09e52ca65e0214fcceda370c12b54d496a1b94
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-01-23 01:25:45 +0000

    Stimulated Data

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,10 @@
 #### Preamble ####
-# Purpose: Simulated Data (IMIT)
+# Purpose: Simulated Data for victims of crime
 # Author: Mohammed Yusuf Shaikh
-# Date: format(Sys.Date(), "%d %B %Y")
+# Date: 18th January 2024
 # Contact: mohammedyusuf.shaikh@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
+# Pre-requisites: None
 
 
 #### Workspace setup ####
@@ -18,24 +17,19 @@ library(janitor)
 library(tibble)
 
 #### Simulate data ####
-# Set the seed for reproducibility
-# Set the seed for reproducibility
-set.seed(250)  
 
-# Setting number of rows
-n_id <- 100
+# Set the seed for reproducibility
+set.seed(853)  
 
-# Creating a simulated data 
+# Creating a simulated data for victims with different crime types
 stimulated_data <- tibble(
-  "Victim_id" = 1:n_id,
-  "REPORT_YEAR" = sample(2014:2022, n_id, replace = TRUE),
-  "CATEGORY" = 'Crimes Against the Person',
-  "SUBTYPE" = sample(c('Sexual Violation', 'Assault', 'Robbery', 'Other'), n_id, replace = TRUE),
-  "ASSAULT_SUBTYPE" = sample(c('Aggravated', 'Non-Aggravated', 'N/A', 'Assault on Peace Officer'), n_id, replace = TRUE),
-  "SEX" = sample(c('M', 'F', 'U'), n_id, replace = TRUE),
-  "AGE_GROUP" = sample(c('Adult', 'Youth', 'Child', 'Unknown'), n_id, replace = TRUE),
-  "AGE_COHORT" = sample(c('<12', '12 to 17', '18 to 24', '25 to 34', '35 to 44', '45 to 54', '55 to 64', '65+', 'Unknown'), n_id, replace = TRUE),
-  "COUNT" = sample(1:200, n_id, replace = TRUE) 
+  "VICTIM_ID" = 1:500,
+  "REPORT_YEAR" = sample(2014:2022, 500, replace = TRUE),
+  "SUBTYPE" = sample(x = c('Sexual Violation', 'Assault', 'Robbery', 'Other'), 500, replace = TRUE),
+  "SEX" = sample(x = c('M', 'F', 'U'), 500, replace = TRUE),
+  "AGE_GROUP" = sample(x = c('Adult', 'Youth', 'Child', 'Unknown'), 500, replace = TRUE),
+  "AGE_COHORT" = sample(x = c('<12', '12 to 17', '18 to 24', '25 to 34', '35 to 44', '45 to 54', '55 to 64', '65+', 'Unknown'), 500, replace = TRUE),
+  "COUNT" = sample(1:200, 500, replace = TRUE) 
 )
 
 # View first & last 6 rows of the stimulated data
