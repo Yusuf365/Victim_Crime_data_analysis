06b6075312cec230b8cfdb8e8a7e0fdb1f14cec3
Mohammed Yusuf Shaikh <yusuf.mns@icloud.com>
2024-01-23 02:15:57 +0000

    Uploading cleaned data

--- scripts/03-test_data.R
+++ scripts/03-test_data.R
@@ -10,6 +10,26 @@
 
 #### Workspace setup ####
 library(tidyverse)
-# [...UPDATE THIS...]
+library(janitor)
+library(dplyr)
+
+#### Access clean data for Testing
+cleaned_victim_data <- read_csv("inputs/data/cleaned_data.csv", show_col_types = FALSE)
+
 
 #### Test data ####
+
+# Test 1 has only 4 unique crime types
+cleaned_victim_data$subtype |>
+  unique() |>
+  length() == 4
+# Test 2 check minimum year in data
+cleaned_victim_data$report_year |> min() == 2014
+
+# Test 3 check maximum year in data
+cleaned_victim_data$report_year |> max() == 2022
+
+# Testing class for victim_count data
+cleaned_victim_data$count |> class() == "numeric"
+
+
